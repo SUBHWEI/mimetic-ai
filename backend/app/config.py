f2483bf2d6eb959a566cc690b82655a21602559b
Julian Correa <julian.correa2809@gmail.com>
2026-05-15 02:38:03 -0500

    Add render.yaml and MONGODB_URL env var support

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,2 +1,4 @@
-MONGODB_URL = "mongodb://localhost:27017"
-MONGODB_DB_NAME = "mimetic_ai"
+import os
+
+MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
+MONGODB_DB_NAME = os.getenv("MONGODB_DB_NAME", "mimetic_ai")
